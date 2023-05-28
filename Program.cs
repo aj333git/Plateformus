77fae3a6602e1e0bec5769846d85ca983def07f5
aj333git <amit.doeacc.joshi@gmail.com>
2023-05-28 21:09:04 +0530

    initial  commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 using Plateformus;
 
+//set up the server
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<GymOptions>(options => {
     options.Machine_Name = "Shoulder_Press";
 });
+//set up the server
 var app = builder.Build();
 app.UseMiddleware<GymMiddleware>();
 
+//Endpoint, run code against client GET using Lambada
 app.MapGet("/", () => "Hello World!");
 
+//run the server
 app.Run();
